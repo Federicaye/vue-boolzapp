d4aedecab4b0b19e4c95dbe7c97d3490c7d61088
Federicaye <gallofederica58@gmail.com>
2024-04-03 23:47:06 +0200

    set timeout

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ createApp({
             activeContact: 1,
             contacts,
             dateLastMsg: null,
+            newMessage: ""
         }
     },
     methods: {
@@ -17,24 +18,50 @@ createApp({
         lastMessage(id) {
             let user = this.contacts.find((el) => el.id === id);
             console.log(user);
-            const { messages} = user;
-            return messages[messages.length-1].message;
+            const { messages } = user;
+            return messages[messages.length - 1].message;
         },
         lastMessageDate(id) {
             let user = this.contacts.find((el) => el.id === id);
             console.log(user);
             const { messages } = user;
-            return messages[messages.length-1].date;
-        } 
+            return messages[messages.length - 1].date;
+        },
+
+        enterMessage(id) {
+            let enterMessage = {
+                date: new Date(),
+                message: this.newMessage,
+                status: 'sent'
+            };
+            let user = this.contacts.find((el) => el.id === this.activeContact);
+            const { messages } = user;
+            messages.push(enterMessage);
+            console.log(enterMessage.message);
+            console.log(enterMessage);
+            setTimeout(()=>{answerMsg(id, user);}, 3000);
+            return enterMessage;
+        },
+
+        answerMsg(id, user){
+            let answerMsg = {
+                date: new Date(),
+                message: "ok",
+                status: 'received'
+            };
+            const { messages } = user;
+            messages.push(answerMsg);
+
+        }
 
     },
     computed: {
         activeContactChat() {
             return this.contacts.find((el) => el.id === this.activeContact)
         }
-      
+
     },
     mounted() {
 
-    } 
+    }
 }).mount('#app')
